4dfb788e283b3e27e1cad11a8abb8db9646f7c3c
kodeX <95200820+kode28003@users.noreply.github.com>
2022-12-18 00:56:28 +0900

    Update getFcmToken.dart

--- getFcmToken.dart
+++ getFcmToken.dart
@@ -1,5 +1,8 @@
 import 'package:fcm_config/fcm_config.dart';
 
+//webで実行すると空になる。
+//push通知など、端末ごとにFCMTokenが必要
+
 Future<String?> getFcmToken() async {
   try {
     final deviceToken = await FCMConfig.instance.messaging.getToken();
